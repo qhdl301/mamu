1c284f6425faec0f6c938145993f436afbf355a4
Young Soo Kim <qhdl301@gmail.com>
2021-07-19 20:22:43 +0900

    codes:tabs 컴포넌트 구현

--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -1,5 +1,6 @@
 import Tabs from './Tabs';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default {
     title: 'components/Tabs',  // title을 사용하면 카테고리를 나눌 수 있다.
@@ -21,40 +22,62 @@ export type TabTypes = {
     }
 }
 
-const [componentData, setcomponentData] = useState<TabTypes>({
-    tabs: {
-        id: '',
-        href: '',
-        ariacontrols :'',
-        value: '',
-    },
-    contents: {
-        id: '',
-        arialabelledby: '',
-        value: '',
-    }
-});
 
 export const Tabs95  = () => {
 
-    setcomponentData(
-    {
+    const [componentData, setcomponentData] = useState<TabTypes[]>([{
         tabs: {
-            id: 'dailyMovies-tab',
-            href: '../#dailyMovies',
-            ariacontrols: 'dailyMovies',
-            value: '일간 무비',
+            id: '',
+            href: '',
+            ariacontrols :'',
+            value: '',
         },
         contents: {
-            id: 'dailyMovies',
-            arialabelledby : 'dailyMovies-tab',
-            value : '일간 무비 내용',
+            id: '',
+            arialabelledby: '',
+            value: '',
         }
-    });
+    }]);
+    
+    useEffect(() => {
+
+        setcomponentData(
+            [
+                {
+                    tabs: {
+                        id: 'dailyMovies-tab',
+                        href: '../#dailyMovies',
+                        ariacontrols: 'dailyMovies',
+                        value: '일간 무비',
+                    },
+                    contents: {
+                        id: 'dailyMovies',
+                        arialabelledby : 'dailyMovies-tab',
+                        value : '일간 무비 내용',
+                    }
+                },
+                {
+                    tabs: {
+                        id: 'dailyMovies-tab',
+                        href: '../#dailyMovies',
+                        ariacontrols: 'dailyMovies',
+                        value: '일간 무비',
+                    },
+                    contents: {
+                        id: 'dailyMovies',
+                        arialabelledby : 'dailyMovies-tab',
+                        value : '일간 무비 내용',
+                    }
+                }
+            ]
+        );      
+              
+    },[])
+    
     
     return (
 
-        <Tabs componentData ={componentData}></Tabs>
+        <Tabs componentData ={componentData}/>
 
     );
 
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -3,25 +3,26 @@ import { TabTypes } from './Tabs.stories';
 import '../../statics/win95.css';
 
 type TabsProps = {
-    componentData: TabTypes,
+    componentData: TabTypes[],
 }
 
 const Tabs:FC<TabsProps> = (props) => {
 
-    const { tabs, contents } = props.componentData;
-
+    const [{ tabs, contents }] = props.componentData;
+    
     return (
         // class component의 class attribute는 className으로 바꿔서 사용가능  
         <>    
             <ul className="nav nav-tabs" id="myTab" role="tablist">
-                <li className="nav-item">
-                    <a className="nav-link active" id={ tabs.id } data-toggle="tab" href={ tabs.href }
-                        role="tab" aria-controls={ tabs.ariacontrols } aria-selected="true">{ tabs.value }</a>
-                    {/*
-                        active : active클래스를 붙여, 활성화된 스타일로 변경한다.
-                        aria-controls : 해당 Elemnet로 제어하는 대상을 식별한다. 연관 된 Element의 id 값을 이용
-                        aria-selected : 선택하여 해당 Elemet를 활성화 한다.  */}
-                </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" id={ tabs.id } data-toggle="tab" href={ tabs.href }
+                            role="tab" aria-controls={ tabs.ariacontrols } aria-selected="true">{ tabs.value }</a>
+                        {/*
+                            active : active클래스를 붙여, 활성화된 스타일로 변경한다.
+                            aria-controls : 해당 Elemnet로 제어하는 대상을 식별한다. 연관 된 Element의 id 값을 이용
+                            aria-selected : 선택하여 해당 Elemet를 활성화 한다.  */}
+                    </li>
+                
             </ul>
             <div className="tab-content" id="myTabContent">
                 <div className="tab-pane show active" id={ contents.id } role="tabpanel" aria-labelledby={ contents.arialabelledby }>     { /* role : Element의 역할을 정의하여 목적을 가지게 한다*/}
