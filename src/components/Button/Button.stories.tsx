7557b7a893819ba0db14a3ed527f00be5eed5182
Young Soo Kim <qhdl301@gmail.com>
2021-07-23 11:59:29 +0900

    codes:Button 컴포넌트 config에 따른 버튼 스타일 변경되도록  개발

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import Button from './Button';
+import * as CONFIGDATA from "./config";
 
 export default {
   title: 'components/Button',  // title을 사용하면 카테고리를 나눌 수 있다.
@@ -6,8 +7,10 @@ export default {
 };
 
 export const Button95 = () => {
+    const { COLOR, SIZE, BORDER } = CONFIGDATA.BUTTON;
+
     return (
-      <Button>BUTTON</Button>
+      <Button color={COLOR} size={SIZE} border={BORDER}>BUTTON</Button>
     )
 }
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,18 @@
 import {FC} from 'react';
-import * as CONFIGDATA from "./config";
 
-const Button:FC = (props)=>{
+type ButtonProps = {
+
+    border : string,
+    color : string,
+    size : string,
+}
+
+const Button:FC<ButtonProps> = (props)=>{
     // rest는 props로 넘어오는 데이터를 객체/배열로 구성시켜주고, destructuring을 통해 rest 객체 중 children 프로퍼티를 별도로 빼서 사용 가능하다. 
-    const { SIZE, COLOR, BORDER } = CONFIGDATA.BUTTON;
-    const { children } = props; // children : 부모 노드의 컴포넌트 값
-
+    const { children, size, color, border } = props; // children : 부모 노드의 컴포넌트 값
+                                         
     return (
-        <button className= {`btn mr-2 mb-2 ${SIZE} btn-${COLOR} ${BORDER}`}>{children}</button>
+        <button className= {`btn mr-2 mb-2 ${size} btn-${color} ${border}`}>{children}</button>
     )
 }
 
