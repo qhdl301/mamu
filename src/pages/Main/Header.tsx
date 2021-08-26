45ed4ba0abfd4689ddb49ca6af21133f7bdfcc47
Young Soo Kim <qhdl301@gmail.com>
2021-08-26 18:00:20 +0900

    codes:타이틀 AppBar 셋팅

--- src/pages/Main/Header.tsx
+++ src/pages/Main/Header.tsx
@@ -1,12 +1,27 @@
 
+import { AppBar, Typography } from '@material-ui/core';
 import { useFireBaseState } from '../../contexts';
 import { Profile } from './components';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+  },
+}));
 
 const Header = () => {
   const loginState = useFireBaseState();
+  const classes = useStyles();
 
   return (
-      <Profile userImg={loginState.user.photoURL} userName={loginState.user.displayName}/>  
+    <AppBar position="static" color="inherit">
+      <Typography variant="h5" color="inherit" className={classes.title}>
+        MAMU
+      </Typography>
+      <Profile userImg={loginState.user.photoURL} userName={loginState.user.displayName} />
+    </AppBar>  
   );
 };
 
